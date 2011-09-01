c85e29efc0885b1d3ea8fd5193bcb0500f5d4882
redgraffix <red@redgraffix.com>
2011-09-01 11:18:07 -0400

    first make
    first build of project

---
+++ extension/prettymusic.js
@@ -0,0 +1,5 @@
+function prettymusic(){	
+$('.gbtc li:eq(0)').after("<li id='prettymusicoff' class='gbt'><a target='_blank' class='gbzt' id='gb_32' href='http://music.google.com'><span class='gbtb2'></span><span class='gbts'>Music</span></a></li>");
+$('a#gb_119.gb1').after("<span id='prettymusic' class='gb1'>Music</span>");
+};
+$(document, window).ready(function() {prettymusic();});
